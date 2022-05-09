c5b531ba450af26d02693f714e470a2da11f79c3
Erik Xu <81636759@qq.com>
2022-05-10 02:15:20 +0800

    Add build script.

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd src/QRCodeService
+
+rm -rf ../../publish
+
+dotnet publish -c Release -o ../../publish -r alpine-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugType=None /p:DebugSymbols=false --self-contained true
