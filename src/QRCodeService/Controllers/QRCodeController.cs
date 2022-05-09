66177e289548d262b8301b69844e99b3e7e55dc5
Erik Xu <81636759@qq.com>
2022-05-10 04:54:19 +0800

    Create qrcode from file.

--- src/QRCodeService/Controllers/QRCodeController.cs
+++ src/QRCodeService/Controllers/QRCodeController.cs
@@ -13,24 +13,39 @@ namespace QRCodeService.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpPost]
-        public async Task<IActionResult> Generate([FromBody] QRCodeForm form)
+        public async Task<IActionResult> Generate([FromForm] QRCodeForm form, IFormFile? file)
         {
             var outDir = Directory.GetCurrentDirectory();
-
             var isUnix = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
             if (isUnix)
             {
                 outDir = "/tmp";
             }
 
-            var filename = $"{Guid.NewGuid()}.png";
-
-            var command = $"amzqr {form.Text} -l {form.Level}";
-
+            var command = $"amzqr {form.Text} -l {form.Level.ToUpper()}";
             if (form.Version != null)
             {
                 command = $"{command} -v {form.Version}";
             }
+
+            var isGif = false;
+
+            if (file != null)
+            {
+                var ext = Path.GetExtension(file.FileName).ToLower();
+                if (ext == ".gif")
+                {
+                    isGif = true;
+                }
+
+                var tmpPath = Path.Combine(outDir, isGif ? $"{Guid.NewGuid()}.gif" : $"{Guid.NewGuid()}.png");
+                await using var stream = System.IO.File.Create(tmpPath);
+                await file.CopyToAsync(stream);
+           
+                command = form.Colorized ? $"{command} -p {tmpPath} -c" : $"{command} -p {tmpPath}";
+            }
+
+            var filename = Path.Combine(outDir, isGif ? $"{Guid.NewGuid()}.gif" : $"{Guid.NewGuid()}.png");
 
             command = $"{command} -n {filename} -d {outDir}";
 
@@ -44,7 +59,8 @@ namespace QRCodeService.Controllers
             var bytes = await System.IO.File.ReadAllBytesAsync(Path.Combine(outDir, filename));
             System.IO.File.Delete(Path.Combine(outDir, filename));
 
-            return File(bytes, "image/png", filename);
+            var contentType = isGif ? "image/gif" : "image/png";
+            return File(bytes, contentType, filename);
         }
 
         private (int, string) ExecuteCommand(string command)
--- src/QRCodeService/QRCodeForm.cs
+++ src/QRCodeService/QRCodeForm.cs
@@ -12,16 +12,42 @@ namespace QRCodeService
         public string? Text { get; set; }
 
         /// <summary>
-        /// 边长，范围是1至40，数字越大边长越大
+        /// 边长，范围是 1 至 40，数字越大边长越大
         /// </summary>
         [Range(1, 40)]
-        [DefaultValue(null)]
         public int? Version { get; set; } = null;
 
         /// <summary>
-        /// 纠错水平，范围是L、M、Q、H，从左到右依次升高，默认是 H
+        /// 纠错水平，范围是 L, M, Q, H，从左到右依次升高，默认是 H
         /// </summary>
         [DefaultValue("H")]
+        [LevelValidation]
         public string Level { get; set; } = "H";
+
+        /// <summary>
+        /// 是否生成彩色二维码，需上传文件
+        /// </summary>
+        public bool Colorized { get; set; } = false;
+    }
+
+    public class LevelValidationAttribute : ValidationAttribute
+    {
+        public string GetErrorMessage() => "Level value should be one of the values: L, M, Q, H";
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not string level)
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            level = level.ToUpper();
+            if (level != "L" && level != "M" && level != "Q" && level != "H")
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
