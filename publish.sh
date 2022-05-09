85f2f0ff855ab56f6a2100291a7b61f01de69640
Erik Xu <81636759@qq.com>
2022-05-10 02:51:35 +0800

    Add pack script.

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ cd src/QRCodeService
 
 rm -rf ../../publish
 
-dotnet publish -c Release -o ../../publish -r alpine-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugType=None /p:DebugSymbols=false --self-contained true
+dotnet publish -c Release -o ../../publish -r alpine-x64 /p:PublishSingleFile=true /p:DebugType=None /p:DebugSymbols=false --self-contained true
